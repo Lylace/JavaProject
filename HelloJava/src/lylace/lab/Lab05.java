39191f4923ac51b2aa05e663c54b49487db7c057
Changhoon Sun_Local <Changhoon Sun_Local@ramona-PC>
2017-12-17 20:42:06 +0900

    집에서 복습

--- HelloJava/src/lylace/lab/Lab05.java
+++ HelloJava/src/lylace/lab/Lab05.java
@@ -37,6 +37,27 @@ public class Lab05 {
 		// 4-7
 		System.out.print("가위(1),바위(2),보(3) 중 하나를 입력하세요.>");
 
-	}
+		sc = new Scanner(System.in);
+		int user = sc.nextInt(); // 화면을 통해 입력받은 숫자를 user에 저장
+		int com = (int) (Math.random() * 3) + 1; // 1, 2 ,3 중 하나가 com에 저장됨
 
-}
+		System.out.println("당신은  " + user + "입니다.");
+		System.out.println("컴은  " + com + "입니다.");
+
+		switch (user - com) {
+		case 2:
+		case -1:
+			System.out.println("당신은 졌습니다.");
+			break;
+		case 1:
+		case -2:
+			System.out.println("당신은 이겼습니다.");
+			break;
+		case 0:
+			System.out.println("비겼습니다.");
+			// break; // 마지막 문장이므로 break를 사용할 필요가 없다.
+		}
+
+	} // end of main
+
+} // end of class
--- HelloJava/src/lylace/lab/Lab06.java
+++ HelloJava/src/lylace/lab/Lab06.java
@@ -5,6 +5,8 @@ public class Lab06 {
 	public static void main(String[] args) {
 		// 자바정석 연습문제 3-1~3, 4-1
 
-	}
+		// # 3-1
 
-}
+	} // end of main
+
+} // end of class
